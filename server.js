fa6686305ce56dad8502cb2189659535f39bd23c
Gerardo Garcia <gerardo.garcia@unosquare.com>
2020-09-12 11:01:45 -0500

    Updated test images

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const fetch = require("node-fetch");
 const { ApolloServer, gql } = require("apollo-server");
-const API_URL = "http://db7b534ba471.ngrok.io";
+const API_URL = "http://08bcba6e67fc.ngrok.io";
 
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
