f1841b0b2c20f8cc60bb02b180d7e12cf112d6ec
Gerardo Garcia <jerry@Gerardos-MacBook-Pro.local>
2021-07-10 21:50:52 -0500

    Updated ngrok url

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const fetch = require("node-fetch");
 const { ApolloServer, gql } = require("apollo-server");
-const API_URL = "http://b2c21520a21e.ngrok.io"; //"http://779e2cbacf41.ngrok.io"; //"http://localhost:3004";
+const API_URL = "http://b2c21520a21e.ngrok.io";
 const GOREST_API_URL = "https://gorest.co.in/public-api/";
-const HOBBIT_API_URL = "http://f0e31345519d.ngrok.io/";
+const HOBBIT_API_URL = "http://e01d38468946.ngrok.io/";
 
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
